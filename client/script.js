18891af8e94385334b8e1f98f4f4782b173d1d9b
Sarveshvarn Ramprasad <71520735+Sarveshvarn@users.noreply.github.com>
2023-03-30 22:00:34 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server ->
 
-  const response = await fetch('https://codex-8yq4.onrender.com', {
+  const response = await fetch('https://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
